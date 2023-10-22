7d9a927bcc75110ccb940760a7b2827d541253b6
Minh Nguyen <ntminh@bgsu.edu>
2023-10-21 20:31:23 -0400

    update

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome the the MATH GAME");
+Console.WriteLine("__________________________");
+Console.WriteLine("Enter your name here");
+string name = Console.ReadLine();
+Console.WriteLine("Welcome " + name + ". This is the math game");
+Console.WriteLine("\n");
+
+Console.WriteLine(@$"What do you want to play today? Choose from the options below:
+A: Addition
+S: Subtraction
+M: Multiplication
+D: Division
+Q: Quit the game");
