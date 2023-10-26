06c95b54679b776f42e6791fbe2c538732a783d8
Minh Nguyen <ntminh@bgsu.edu>
2023-10-26 13:08:56 -0400

    changes

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(1 + " string. example: "+"Mike");
+
+
+Console.WriteLine("2" + "2");
